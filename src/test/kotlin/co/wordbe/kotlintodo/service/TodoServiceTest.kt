7840ca521cf79c5389943207e0bdf0e380ffb3ea
Wordbe <seonghojin3@gmail.com>
2022-11-22 14:29:18 +0900

    feat: todo service test

--- src/test/kotlin/co/wordbe/kotlintodo/service/TodoServiceTest.kt
+++ src/test/kotlin/co/wordbe/kotlintodo/service/TodoServiceTest.kt
@@ -24,28 +24,27 @@ class TodoServiceTest {
     @MockK
     lateinit var todoRepository: TodoRepository
 
-    private val stub: Todo by lazy {
-        Todo(
-            id = 1,
-            title = "테스트",
-            description = "테스트 상세",
-            done = false,
-            priority = TodoPriority.LOW,
-            createdAt = LocalDateTime.now(),
-            updatedAt = LocalDateTime.now(),
-        )
-    }
-
     @DisplayName("한 개의 TODO 를 반환해야 한다")
     @Test
     fun `return a todo`() {
-        every { todoRepository.findByIdOrNull(1) } returns stub
+        val todo = todo()
+        every { todoRepository.findByIdOrNull(1) } returns todo
 
         val result = todoService.findById(1)
 
         assertThat(result).isNotNull
-        assertThat(result).isEqualTo(stub)
+        assertThat(result).isEqualTo(todo)
 
         verify(exactly = 0) { todoService.findAll() }
     }
+
+    private fun todo() = Todo(
+        id = 1,
+        title = "테스트",
+        description = "테스트 상세",
+        done = false,
+        priority = TodoPriority.LOW,
+        createdAt = LocalDateTime.now(),
+        updatedAt = null,
+    )
 }
