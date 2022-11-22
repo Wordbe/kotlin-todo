da5e8ec04eeb2b1b1b3d026f8f78a51b6550c656
Wordbe <seonghojin3@gmail.com>
2022-11-22 14:21:52 +0900

    feat: todo service test

--- src/test/kotlin/co/wordbe/kotlintodo/service/TodoServiceTest.kt
+++ src/test/kotlin/co/wordbe/kotlintodo/service/TodoServiceTest.kt
@@ -1,11 +1,13 @@
 package co.wordbe.kotlintodo.service
 
 import co.wordbe.kotlintodo.domain.Todo
+import co.wordbe.kotlintodo.domain.TodoPriority
 import co.wordbe.kotlintodo.domain.TodoRepository
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
+import io.mockk.verify
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -17,10 +19,10 @@ import java.time.LocalDateTime
 class TodoServiceTest {
 
     @InjectMockKs
-    lateinit var service: TodoService
+    lateinit var todoService: TodoService
 
     @MockK
-    lateinit var repository: TodoRepository
+    lateinit var todoRepository: TodoRepository
 
     private val stub: Todo by lazy {
         Todo(
@@ -28,6 +30,7 @@ class TodoServiceTest {
             title = "테스트",
             description = "테스트 상세",
             done = false,
+            priority = TodoPriority.LOW,
             createdAt = LocalDateTime.now(),
             updatedAt = LocalDateTime.now(),
         )
@@ -36,11 +39,13 @@ class TodoServiceTest {
     @DisplayName("한 개의 TODO 를 반환해야 한다")
     @Test
     fun `return a todo`() {
-        every { repository.findByIdOrNull(1) } returns stub
+        every { todoRepository.findByIdOrNull(1) } returns stub
 
-        val result = service.findById(1)
+        val result = todoService.findById(1)
 
         assertThat(result).isNotNull
         assertThat(result).isEqualTo(stub)
+
+        verify(exactly = 0) { todoService.findAll() }
     }
 }
